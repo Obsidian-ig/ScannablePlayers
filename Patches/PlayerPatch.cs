948523aae27af164ac79ec44af0e4c457710c8aa
Obsidian-ig <villocityontiktok@gmail.com>
2023-11-28 09:15:28 -0800

    Uploaded Source Code

---
+++ Patches/PlayerPatch.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Windows;
+
+namespace LethalCompanyMods.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void ScannablePlayers()
+        {
+
+            List<int> playerIds = new List<int>();
+
+            foreach (UnityEngine.GameObject player in UnityEngine.GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if (!player.GetComponent<ScanNodeProperties>())
+                {
+                    player.AddComponent<ScanNodeProperties>();
+                    UnityEngine.GameObject.Instantiate(UnityEngine.GameObject.Find("ScanNode")).transform.parent = player.transform;
+                    player.GetComponent<ScanNodeProperties>().headerText = player.name;
+                    player.GetComponent<ScanNodeProperties>().scrapValue = 0;
+                    player.GetComponent<ScanNodeProperties>().requiresLineOfSight = false;
+                    player.GetComponent<ScanNodeProperties>().subText = player.name;
+                    player.GetComponent<ScanNodeProperties>().maxRange = 999;
+                    player.GetComponent<ScanNodeProperties>().minRange = 0;
+                    playerIds.Add(player.GetInstanceID());
+                    //player.GetComponent<ScanNodeProperties>().creatureScanID = null;
+                }
+            }
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using LethalCompanyMods.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannablePlayersMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+
+    public class ModBase : BaseUnityPlugin
+    {
+
+        private const string modGUID = "Obsidian.Lithium.ScannablePlayers";
+        private const string modName = "Lithium";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
